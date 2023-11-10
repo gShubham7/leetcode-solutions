fded6077f5cfbd68ff608d2a5678d330bd05e0dd
shubham <gaikwad7777@gmail.com>
2023-11-10 23:47:27 +0530

    move zeroes solution added

---
+++ 283.move-zeroes.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=283 lang=javascript
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let nonZeroIndex = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != 0) {
+      nums[nonZeroIndex] = nums[i];
+      nonZeroIndex++;
+    }
+  }
+
+  for (let i = nonZeroIndex; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+};
+// Time Complexity  -----> O(n);
+// Space Complexity  -----> O(n);
+// @lc code=end
