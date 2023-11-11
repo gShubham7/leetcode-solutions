6efcac4a1bbfa63410076e2e0bc72c381b0e8614
shubham <gaikwad7777@gmail.com>
2023-11-11 19:34:24 +0530

    insert-delete-get-random-o-1-solution-added

---
+++ 380.insert-delete-get-random-o-1.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=380 lang=javascript
+ *
+ * [380] Insert Delete GetRandom O(1)
+ */
+
+// @lc code=start
+
+var RandomizedSet = function () {
+  this.set = new Set();
+};
+
+/**
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function (val) {
+  if (this.set.has(val)) {
+    return false;
+  }
+
+  this.set.add(val);
+  return true;
+};
+
+/**
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function (val) {
+  if (!this.set.has(val)) {
+    return false;
+  }
+
+  this.set.delete(val);
+  return true;
+};
+
+/**
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function () {
+  const randomIndex = Math.floor(Math.random() * this.set.size);
+  return [...this.set][randomIndex];
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
+// @lc code=end
