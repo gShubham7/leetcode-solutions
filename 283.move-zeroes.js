6faab096c08a3b6cf0afff9add18337e4d0301a0
shubham <gaikwad7777@gmail.com>
2023-11-11 19:10:43 +0530

    move-zeroes-solution-two-pointer-added

--- 283.move-zeroes.js
+++ 283.move-zeroes.js
@@ -10,19 +10,34 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-  let nonZeroIndex = 0;
+  //   let nonZeroIndex = 0;
+
+  //   for (let i = 0; i < nums.length; i++) {
+  //     if (nums[i] != 0) {
+  //       nums[nonZeroIndex] = nums[i];
+  //       nonZeroIndex++;
+  //     }
+  //   }
+
+  //   for (let i = nonZeroIndex; i < nums.length; i++) {
+  //     nums[i] = 0;
+  //   }
+  // };
+  // Time Complexity  -----> O(n);
+  // Space Complexity  -----> O(1);
+
+  //Single Traversal of array -----> Two pointer
+  let j = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] != 0) {
-      nums[nonZeroIndex] = nums[i];
-      nonZeroIndex++;
+    if (nums[i] != 0 && nums[j] == 0) {
+      let temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
     }
-  }
-
-  for (let i = nonZeroIndex; i < nums.length; i++) {
-    nums[i] = 0;
+    if (nums[j] != 0) j++;
   }
 };
 // Time Complexity  -----> O(n);
-// Space Complexity  -----> O(n);
+// Space Complexity  -----> O(1);
 // @lc code=end
