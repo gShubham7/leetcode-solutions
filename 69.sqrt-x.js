71b8531133eb4a254ea3b5302f7cba2f8b8a6085
shubham <gaikwad7777@gmail.com>
2023-11-12 22:52:32 +0530

    sqrt-x-solution-with-binary-search-added

--- 69.sqrt-x.js
+++ 69.sqrt-x.js
@@ -10,6 +10,24 @@
  * @return {number}
  */
 var mySqrt = function (x) {
-  return Math.floor(Math.sqrt(x));
+  // Binary search
+  let left = 0;
+  let right = x;
+  if (x <= 1) {
+    return x;
+  }
+  while (left < right) {
+    let mid = Math.floor((right + left) / 2);
+    let square = mid * mid;
+    if (square === x) return mid;
+    if (square < x) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return left - 1;
+  // Time Complexity -----> O(log(n))
+  // Space Complexity  -----> O(1)
 };
 // @lc code=end
