42eea845c11d0c1122df0382270e787bc471fba1
shubham <gaikwad7777@gmail.com>
2023-11-22 19:53:21 +0530

    4sum-solution-added

---
+++ 18.4-sum.js
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=18 lang=javascript
+ *
+ * [18] 4Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function (nums, target) {
+  if (nums.length < 4) {
+    return [];
+  }
+
+  nums.sort((a, b) => a - b);
+
+  let res = [];
+
+  for (let i = 0; i < nums.length - 3; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+    for (let j = i + 1; j < nums.length - 2; j++) {
+      if (j > i + 1 && nums[j] === nums[j - 1]) {
+        continue;
+      }
+
+      let left = j + 1;
+      let right = nums.length - 1;
+
+      while (left < right) {
+        const sum = nums[i] + nums[j] + nums[left] + nums[right];
+        if (sum < target) {
+          left++;
+        } else if (sum > target) {
+          right--;
+        } else {
+          res.push([nums[i], nums[j], nums[left], nums[right]]);
+
+          while (left < right && nums[left] === nums[left + 1]) {
+            left++;
+          }
+
+          while (left < right && nums[right] === nums[right - 1]) {
+            right--;
+          }
+
+          left++;
+          right--;
+        }
+      }
+    }
+  }
+
+  return res;
+  // Time Complexity    -----> O(n^3)
+  // Space Complexity   -----> O(1)
+};
+// @lc code=end
