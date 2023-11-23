bd48f66a121ce7080f443a526d0f49a1e7f5641e
shubham <gaikwad7777@gmail.com>
2023-11-23 23:20:25 +0530

    binary-search-solution-added

--- 35.search-insert-position.js
+++ 35.search-insert-position.js
@@ -11,13 +11,34 @@
  * @return {number}
  */
 var searchInsert = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    if (target <= nums[i]) {
-      return i;
+  //   for (let i = 0; i < nums.length; i++) {
+  //     if (target <= nums[i]) {
+  //       return i;
+  //     }
+  //   }
+  //   return nums.length;
+  // Time Complexity    -----> O(n)
+  // Space Complexity   -----> O(1)
+
+  // Binary Search
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (nums[mid] == target) {
+      return mid;
+    }
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
   }
-  return nums.length;
-  // Time Complexity    -----> O(n)
+  return left;
+  // Time Complexity    -----> O(log n)
   // Space Complexity   -----> O(1)
 };
 // @lc code=end
