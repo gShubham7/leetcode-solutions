76c46abffbdf56aebaafc4148d7c94b0d22146ba
shubham <gaikwad7777@gmail.com>
2023-11-24 19:00:33 +0530

    optimized-approach

--- 151.reverse-words-in-a-string.js
+++ 151.reverse-words-in-a-string.js
@@ -10,17 +10,47 @@
  * @return {string}
  */
 var reverseWords = function (s) {
-  let str = s.split(" ");
-  let res = "";
+  //   let str = s.split(" ");
+  //   let res = "";
 
-  for (let i = str.length - 1; i >= 0; i--) {
-    if (str[i]) {
-      res += (res ? " " : "") + str[i];
-    }
-  }
+  //   for (let i = str.length - 1; i >= 0; i--) {
+  //     if (str[i]) {
+  //       res += (res ? " " : "") + str[i];
+  //     }
+  //   }
 
-  return res;
+  //   return res;
   // Time Complexity    -----> O(n)
   // Space Complexity   -----> O(n)
+
+  // Optimized Approach
+
+  let word = "";
+  let right = s.length - 1,
+    left = right;
+
+  while (right >= 0) {
+    while (s[right] === " ") {
+      --right;
+      --left;
+    }
+
+    while (left >= 0 && s[left] !== " ") {
+      --left;
+    }
+
+    let i = left + 1;
+    while (i <= right) {
+      if (i == left + 1 && word !== "") {
+        word += " ";
+      }
+      word += s[i];
+      ++i;
+    }
+    right = left;
+  }
+  return word;
+  // Time Complexity    -----> O(n)
+  // Space Complexity   -----> O(1)
 };
 // @lc code=end
