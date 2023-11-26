5b962fffdd72f05d2d32977818620e6627856a9a
shubham <gaikwad7777@gmail.com>
2023-11-26 12:07:37 +0530

    roman-to-integer-solution added

---
+++ 13.roman-to-integer.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=13 lang=javascript
+ *
+ * [13] Roman to Integer
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  const hash = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let res = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const cur = hash[s[i]];
+    const next = hash[s[i + 1]];
+
+    if (cur < next) {
+      res += next - cur;
+      i++;
+    } else {
+      res += cur;
+    }
+  }
+
+  return res;
+  // Time Complexity    -----> O(n)
+  // Space Complexity   -----> O(1)
+};
+// @lc code=end
