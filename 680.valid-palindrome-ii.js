6047c9dda75f287823d044383457e3f05fc99e6d
shubham <gaikwad7777@gmail.com>
2023-11-26 11:18:40 +0530

    valid-palindrome-ii-solution-added

---
+++ 680.valid-palindrome-ii.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=680 lang=javascript
+ *
+ * [680] Valid Palindrome II
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return (
+        isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1)
+      );
+    }
+    left++;
+    right--;
+  }
+  return true;
+
+  function isPalindrome(s, i, j) {
+    while (i < j) {
+      if (s[i] !== s[j]) {
+        return false;
+      }
+      i++;
+      j--;
+    }
+    return true;
+  }
+  // Time Complexity    -----> O(n)
+  // Space Complexity   -----> O(1)
+};
+// @lc code=end
