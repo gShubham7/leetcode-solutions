c9114f864a82791fc710487571868a9cd62523f0
shubham <gaikwad7777@gmail.com>
2023-11-29 22:40:52 +0530

    reverse-string-solution-added

---
+++ 344.reverse-string.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=344 lang=javascript
+ *
+ * [344] Reverse String
+ */
+
+// @lc code=start
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    let temp = str[left];
+    str[left] = str[right];
+    str[right] = temp;
+    left++;
+    right--;
+  }
+  return str;
+
+  // Time Complexity    -----> O(n)
+  // Space Complexity    -----> O(1)
+};
+// @lc code=end
