69bb28a56504c85a8dbe045ffedfacb491fef7ec
shubham <gaikwad7777@gmail.com>
2023-11-30 20:15:44 +0530

    find-the-duplicate-number-solution-added

---
+++ 287.find-the-duplicate-number.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=287 lang=javascript
+ *
+ * [287] Find the Duplicate Number
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function (nums) {
+  let set = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      return nums[i];
+    }
+    set.add(nums[i]);
+  }
+
+  // Time Complexity    -----> O(n)
+  // Space Complexity   -----> O(n)
+};
+// @lc code=end
