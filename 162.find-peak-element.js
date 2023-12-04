66bdd20385481cb76b263d89b6c8d0d23cf23482
shubham <gaikwad7777@gmail.com>
2023-12-04 21:48:25 +0530

    optimised-solution-added

--- 162.find-peak-element.js
+++ 162.find-peak-element.js
@@ -10,11 +10,28 @@
  * @return {number}
  */
 var findPeakElement = function (nums) {
+  /*
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] > nums[i + 1]) return i;
   }
   return nums.length - 1;
   // Time Complexity    -----> O(n)
+  // Space Complexity   -----> O(1)
+*/
+
+  // Binary Search
+
+  let left = 0,
+    right = nums.length - 1,
+    mid;
+
+  while (left < right) {
+    mid = Math.floor((right + left) / 2);
+    if (nums[mid] > nums[mid + 1]) right = mid;
+    else left = mid + 1;
+  }
+  return left;
+  // Time Complexity    -----> O(log n)
   // Space Complexity   -----> O(1)
 };
 // @lc code=end
