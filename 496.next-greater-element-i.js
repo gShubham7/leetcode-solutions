bfa7adb53f662d6466c25feef91943b45416c7f3
shubham <gaikwad7777@gmail.com>
2023-12-25 22:08:10 +0530

    next-greater-element-i-solution-added

---
+++ 496.next-greater-element-i.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=496 lang=javascript
+ *
+ * [496] Next Greater Element I
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function (nums1, nums2) {
+  const map = new Map();
+  const stack = [];
+  for (let num of nums2) {
+    while (stack.length && stack[stack.length - 1] < num) {
+      map.set(stack.pop(), num);
+    }
+    stack.push(num);
+  }
+  for (let i = 0; i < nums1.length; i++) {
+    nums1[i] = map.has(nums1[i]) ? map.get(nums1[i]) : -1;
+  }
+  return nums1;
+};
+// @lc code=end
