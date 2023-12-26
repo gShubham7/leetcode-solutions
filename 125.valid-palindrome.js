385f2db654fff07186a91fe58f0483347d8ac50a
shubham <gaikwad7777@gmail.com>
2023-12-26 23:33:33 +0530

    valid-palindrome-solution-added

---
+++ 125.valid-palindrome.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=125 lang=javascript
+ *
+ * [125] Valid Palindrome
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  let left = 0,
+    right = s.length - 1;
+  while (left < right) {
+    if (!isAlphaNumeric(s[left])) left++;
+    else if (!isAlphaNumeric(s[right])) right--;
+    else if (s[left].toLowerCase() !== s[right].toLowerCase()) return false;
+    else {
+      left++;
+      right--;
+    }
+  }
+  return true;
+};
+
+function isAlphaNumeric(char) {
+  const code = char.charCodeAt(0);
+  return (
+    (code >= 48 && code <= 57) ||
+    (code >= 65 && code <= 90) ||
+    (code >= 97 && code <= 122)
+  );
+}
+// @lc code=end
