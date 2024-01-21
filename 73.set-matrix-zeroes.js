9e9d7ca950d57a91c7c590787cfcf96ffc6b2e55
shubham <gaikwad7777@gmail.com>
2024-01-21 23:41:00 +0530

    set-matrix-zeroes-solution-added

---
+++ 73.set-matrix-zeroes.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=73 lang=javascript
+ *
+ * [73] Set Matrix Zeroes
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function (matrix) {
+  let rowList = [];
+  let colList = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0) {
+        rowList.push(i);
+        colList.push(j);
+      }
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (rowList.includes(i) || colList.includes(j)) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  return matrix;
+};
+// @lc code=end
