3d80fad92e5e44e74381c4be5f3e9637cc9f7828
shubham <gaikwad7777@gmail.com>
2024-02-05 22:02:37 +0530

    two-sum-ii-input-array-is-sorted-solution-added

---
+++ 167.two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=167 lang=javascript
+ *
+ * [167] Two Sum II - Input Array Is Sorted
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  let left = 0;
+  right = numbers.length - 1;
+
+  while (left < right) {
+    if (numbers[left] + numbers[right] < target) {
+      left++;
+    } else if (numbers[left] + numbers[right] > target) {
+      right--;
+    } else {
+      return [left + 1, right + 1];
+    }
+  }
+};
+// @lc code=end
