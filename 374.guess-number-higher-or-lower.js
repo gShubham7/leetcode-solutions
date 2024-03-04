0178f9722ef74ff28d550437a26b7ecb566fa5a9
shubham <gaikwad7777@gmail.com>
2024-03-04 18:04:03 +0530

    guess-number-higher-or-lower-solution-added

---
+++ 374.guess-number-higher-or-lower.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=374 lang=javascript
+ *
+ * [374] Guess Number Higher or Lower
+ */
+
+// @lc code=start
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function (n) {
+  let start = 1,
+    end = n;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    let guessNum = guess(mid);
+    if (guessNum == 0) {
+      return mid;
+    } else if (guessNum == 1) {
+      start = mid + 1;
+    } else if (guessNum == -1) {
+      end = mid - 1;
+    }
+  }
+};
+// @lc code=end
